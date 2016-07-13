83c21dc5003b960dc168b16dcc083812bd933505
Will Roberts <will.roberts11@gmail.com>
2016-07-13 02:04:40 -0700

    updates assets

---
+++ assets/tilesets/floors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Floors" tilewidth="20" tileheight="20" tilecount="260">
+  <image source="../spritesheets/floor-tiles-20x20.png" width="400" height="260"/>
+</tileset>
