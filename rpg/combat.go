6909e87a169cbe4358955231d571cc487eb00a51
Will Roberts <will.roberts11@gmail.com>
2016-07-16 16:35:56 -0700

    rip out combat system

--- rpg/combat.go
+++ rpg/combat.go
@@ -1,58 +1,10 @@
 // combat.go
 package rpg
 
-import (
-	"log"
-
-	"engo.io/ecs"
-	"engo.io/engo"
-	"engo.io/engo/common"
-)
-
-type CombatSystem struct{}
-
-// FIXME: Player is moved to the tile under the enemy on attack.
-func (c *CombatSystem) New(*ecs.World) {
-	collisionHandler := func(message engo.Message) {
-		msg, isCollision := message.(common.CollisionMessage)
-		if isCollision {
-			// Create a combat event.
-			combatMessage := engo.Message(CombatMessage{
-				Initiator: msg.Entity.ID(),
-				Target:    msg.To.ID(),
-			})
-			engo.Mailbox.Dispatch(combatMessage)
-
-			// TODO: Cancel movement.
-		}
-	}
-	engo.Mailbox.Listen("CollisionMessage", collisionHandler)
-
-	combatHandler := func(message engo.Message) {
-		if message.Type() == "CombatMessage" {
-			msg, _ := message.(CombatMessage)
-			if msg.Initiator == playerEntityID {
-				log.Println("You attacked!")
-			}
-			if msg.Target == playerEntityID {
-				log.Println("You were hit!")
-				// TODO: Deduct hit points.
-			}
-		}
-	}
-	engo.Mailbox.Listen("CombatMessage", combatHandler)
+// Given pointers to two entities, resolve combat between them
+func InitiateCombat(c1, c2 Character) {
 }
 
-func (c *CombatSystem) Remove(ecs.BasicEntity) {}
-
-func (c *CombatSystem) Update(float32) {
-}
-
-type CombatMessage struct {
-	Initiator uint64
-	Target    uint64
-}
-
-func (c CombatMessage) Type() string {
-	return "CombatMessage"
+// When a character's health reaches zero, destroy it
+func DestroyCharacter(c Character) {
 }
