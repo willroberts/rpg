675a43247b8a09e4e184774455dcacf054418f50
Will Roberts <will.roberts11@gmail.com>
2016-07-16 15:33:31 -0700

    move grid into its own file

---
+++ rpg/grid.go
@@ -0,0 +1,52 @@
+// grid.go
+package rpg
+
+import (
+	"log"
+
+	"engo.io/ecs"
+	"engo.io/engo"
+	"engo.io/engo/common"
+)
+
+var (
+	grid *Grid
+)
+
+// Grid is an array of arrays of cells. Cells may hold one entity pointer, and
+// an array of item pointers.
+type Grid struct {
+	Rows []*GridRow
+
+	MinX int
+	MaxX int
+	MinY int
+	MaxY int
+}
+
+type GridRow struct {
+	Cells []*GridCell
+}
+
+type GridCell struct {
+	X, Y int
+
+	Character *Character
+	Items     []*Item
+}
+
+func NewGrid(x, y int) *Grid {
+	rows := make([]*GridRow, x)
+	for i := 0; i < x; i++ {
+		cells := make([]*GridCell, y)
+		for j := 0; j < y; j++ {
+			cells[j] = &GridCell{X: j, Y: i}
+		}
+		rows[i] = &GridRow{Cells: cells}
+	}
+	return &Grid{Rows: rows, MinX: 0, MaxX: x - 1, MinY: 0, MaxY: y - 1}
+}
+
+func (g *Grid) GetCell(x, y int) *GridCell {
+	return g.Rows[y].Cells[x]
+}
--- rpg/mapping.go
+++ rpg/mapping.go
@@ -10,48 +10,9 @@ import (
 )
 
 var (
-	grid        *Grid
 	levelWidth  float32
 	levelHeight float32
 )
-
-// Grid is an array of arrays of cells. Cells may hold one entity pointer, and
-// an array of item pointers.
-type Grid struct {
-	Rows []*GridRow
-
-	MinX int
-	MaxX int
-	MinY int
-	MaxY int
-}
-
-type GridRow struct {
-	Cells []*GridCell
-}
-
-type GridCell struct {
-	X, Y int
-
-	Character *Character
-	Items     []*Item
-}
-
-func NewGrid(x, y int) *Grid {
-	rows := make([]*GridRow, x)
-	for i := 0; i < x; i++ {
-		cells := make([]*GridCell, y)
-		for j := 0; j < y; j++ {
-			cells[j] = &GridCell{X: j, Y: i}
-		}
-		rows[i] = &GridRow{Cells: cells}
-	}
-	return &Grid{Rows: rows, MinX: 0, MaxX: x - 1, MinY: 0, MaxY: y - 1}
-}
-
-func (g *Grid) GetCell(x, y int) *GridCell {
-	return g.Rows[y].Cells[x]
-}
 
 type Tile struct {
 	ecs.BasicEntity
