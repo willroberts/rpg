320c23c796f8d987ede2fcc0f4bfcecba3a56bdf
Will Roberts <will.roberts11@gmail.com>
2016-07-16 17:05:47 -0700

    add debug logs

--- rpg/combat.go
+++ rpg/combat.go
@@ -1,8 +1,13 @@
 // combat.go
 package rpg
 
+import (
+	"log"
+)
+
 // Given pointers to two entities, resolve combat between them
 func InitiateCombat(c1, c2 Character) {
+	log.Println("[combat] initiating combat!")
 }
 
 // When a character's health reaches zero, destroy it
