2b518853df2babafee9ca2ea5a013898682e05ba
Will Roberts <will.roberts11@gmail.com>
2016-07-18 22:03:03 -0700

    Update docstrings

--- rpg/character.go
+++ rpg/character.go
@@ -2,28 +2,22 @@
 
 // RPG: A 2D game written in Go, with the engo engine.
 // Copyright (C) 2016 Will Roberts
-
+//
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 3 of the License, or
 // (at your option) any later version.
-
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software Foundation,
 // Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-// Terminology:
-// 	Character: Anything which can move, interact, attack, etc.
-// 	Player: The player-controlled character
-// 	NPC: Non-hostile characters like shopkeepers
-// 	Enemy: Hostile NPCs
-// Variables and constants named "character" should be applicable to both the
-// player and enemies/NPCs.
+// Package rpg contains all systems needed to run our 2D RPG.
 package rpg
 
 import "engo.io/engo/common"
