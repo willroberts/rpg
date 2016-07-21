1b060160e3a4e1e7142808f6c0b98ecad619f17e
Will Roberts <will.roberts11@gmail.com>
2016-07-21 00:46:55 -0700

    add mapcheck command

---
+++ cmd/mapcheck/main.go
@@ -0,0 +1,62 @@
+// mapcheck
+// Verify the tile layer data from a .tmx file. The XML might look like this:
+//
+//		<layer name="foo" width="12" height="24">
+//			<data encoding="base64" compression="zlib">
+//				eJxjZGBgYBzFo3gUj+IBwACO3gEh
+//			</data>
+//		</layer>
+//
+// When decoded and decompressed, this data turns into a vector of 1s which
+// represent tiles. No structure data is encoded, so the game engine must read
+// from the layer metadata to determine structure. All we can verify at this stage
+// is that the number of tiles is correctly determined.
+package main
+
+import (
+	"bytes"
+	"compress/zlib"
+	"encoding/base64"
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+var (
+	mapData       string = "eJxjZGBgYBzFo3gUj+IBwACO3gEh"
+	expectedTiles int    = 288 // 12x24
+)
+
+func verifyTileCount() {
+	b := []byte(string(mapData))
+	if _, err := base64.StdEncoding.Decode(b, b); err != nil {
+		panic(err)
+	}
+	r := bytes.NewReader(b)
+	z, err := zlib.NewReader(r)
+	if err != nil {
+		panic(err)
+	}
+	defer z.Close()
+	markers := make([]uint32, 0)
+	var next uint32
+	for {
+		err = binary.Read(z, binary.LittleEndian, &next)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		markers = append(markers, next)
+	}
+	if len(markers) == expectedTiles {
+		fmt.Println("[PASS] Detected 288/288 tiles.")
+	} else {
+		fmt.Printf("[FAIL] Detected %d/288 tiles\n.", len(markers))
+	}
+}
+
+func main() {
+	verifyTileCount()
+}
