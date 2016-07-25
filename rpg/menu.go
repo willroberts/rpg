447f8537a94fe873fb34452c408eb9d142c99420
Will Roberts <will.roberts11@gmail.com>
2016-07-25 01:21:21 -0700

    finish menu layout - not interactive yet

--- rpg/menu.go
+++ rpg/menu.go
@@ -49,23 +49,34 @@ func (s *MenuScene) Setup(w *ecs.World) {
 
 	// Create title label and "Portrait:" label.
 	// FIXME: Determine width automatically - via t.SpaceComponent.Width?
-	tl := NewLabel("Game Title", gameFonts.TitleFont, 200, 64)
-	pl := NewLabel("Portrait:", gameFonts.MenuFont, 32, 160)
+	labels := []*Label{
+		NewLabel("Create Character", gameFonts.TitleFont, 128, 50),
+		NewLabel("Name:", gameFonts.MenuFont, 40, 150),
+		NewLabel("Portrait:", gameFonts.MenuFont, 40, 230),
+		NewLabel("Attributes:", gameFonts.MenuFont, 40, 380),
+		NewLabel("STR: XX", gameFonts.MenuFont, 180, 460),
+		NewLabel("DEX: XX", gameFonts.MenuFont, 180, 540),
+		NewLabel("INT: XX", gameFonts.MenuFont, 400, 460),
+		NewLabel("VIT: XX", gameFonts.MenuFont, 400, 540),
+		NewLabel("Start", gameFonts.TitleFont, 280, 620),
+	}
 
 	// draw a character image selector
 	csp := NewCharSelectPanel()
 
 	// draw a character stats selector
 
-	// draw a "Play" button
-
 	// render everything
 	for _, sys := range w.Systems() {
 		switch s := sys.(type) {
 		case *common.RenderSystem:
-			s.Add(&tl.BasicEntity, &tl.RenderComponent, &tl.SpaceComponent)
-			s.Add(&pl.BasicEntity, &pl.RenderComponent, &pl.SpaceComponent)
+			for _, l := range labels {
+				s.Add(&l.BasicEntity, &l.RenderComponent, &l.SpaceComponent)
+			}
 			s.Add(&csp.BasicEntity, &csp.RenderComponent, &csp.SpaceComponent)
+			for _, p := range csp.Portraits {
+				s.Add(&p.BasicEntity, &p.RenderComponent, &p.SpaceComponent)
+			}
 		}
 	}
 }
--- rpg/portraits.go
+++ rpg/portraits.go
@@ -24,25 +24,70 @@ import (
 	"engo.io/engo/common"
 )
 
+type Portrait struct {
+	Name string
+
+	ecs.BasicEntity
+	common.RenderComponent
+	common.SpaceComponent
+}
+
+func NewPortrait(name string, sprite int, x, y float32) *Portrait {
+	p := &Portrait{
+		Name:        name,
+		BasicEntity: ecs.NewBasic(),
+	}
+	p.RenderComponent = common.RenderComponent{
+		Drawable: gameSprites.Characters.Cell(sprite),
+		Scale:    engo.Point{charScale, charScale},
+	}
+	p.RenderComponent.SetZIndex(1)
+	p.SpaceComponent = common.SpaceComponent{
+		Position: engo.Point{x, y},
+		Width:    charSizeX,
+		Height:   charSizeY,
+	}
+	return p
+}
+
 type CharSelectPanel struct {
+	Portraits []*Portrait
+
 	ecs.BasicEntity
 	common.RenderComponent
 	common.SpaceComponent
 }
 
 func NewCharSelectPanel() *CharSelectPanel {
-	p := &CharSelectPanel{
+	c := &CharSelectPanel{
 		BasicEntity: ecs.NewBasic(),
 		RenderComponent: common.RenderComponent{
 			Drawable: common.Rectangle{},
-			Color:    color.RGBA{20, 20, 20, 255},
+			Color:    color.RGBA{35, 35, 35, 255},
 		},
 	}
-	p.SetShader(common.HUDShader)
-	p.SpaceComponent = common.SpaceComponent{
-		Position: engo.Point{32, 200},
-		Width:    320,
-		Height:   160,
+	c.RenderComponent.SetZIndex(0)
+	c.SetShader(common.HUDShader)
+	c.SpaceComponent = common.SpaceComponent{
+		Position: engo.Point{120, 280},
+		Width:    490,
+		Height:   80,
 	}
-	return p
+	c.AddPortraits()
+	return c
+}
+
+func (c *CharSelectPanel) AddPortraits() {
+	c.Portraits = append(c.Portraits, NewPortrait("Human", spriteHuman,
+		134, 284))
+	c.Portraits = append(c.Portraits, NewPortrait("Zombie", spriteGreenZombie,
+		216, 284))
+	c.Portraits = append(c.Portraits, NewPortrait("Zombie", spriteOliveZombie,
+		296, 284))
+	c.Portraits = append(c.Portraits, NewPortrait("Zombie", spriteWhiteZombie,
+		376, 284))
+	c.Portraits = append(c.Portraits, NewPortrait("Mummy", spriteMummy,
+		456, 284))
+	c.Portraits = append(c.Portraits, NewPortrait("Gargoyle", spriteGargoyle,
+		536, 284))
 }
