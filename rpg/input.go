4a971613c6ebf89f0b809746ef267685c8be3209
Will Roberts <will.roberts11@gmail.com>
2018-06-20 00:39:25 -0700

    Updates names for key constants

--- rpg/input.go
+++ rpg/input.go
@@ -4,8 +4,8 @@ import "engo.io/engo"
 
 // bindControls assigns the arrow keys to Player movement.
 func bindControls() {
-	engo.Input.RegisterButton("moveup", engo.ArrowUp)
-	engo.Input.RegisterButton("movedown", engo.ArrowDown)
-	engo.Input.RegisterButton("moveleft", engo.ArrowLeft)
-	engo.Input.RegisterButton("moveright", engo.ArrowRight)
+	engo.Input.RegisterButton("moveup", engo.KeyArrowUp)
+	engo.Input.RegisterButton("movedown", engo.KeyArrowDown)
+	engo.Input.RegisterButton("moveleft", engo.KeyArrowLeft)
+	engo.Input.RegisterButton("moveright", engo.KeyArrowRight)
 }
