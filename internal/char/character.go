ecacc2af10bed5c3c03f3369217e2ae7583df35b
Will Roberts <will.roberts11@gmail.com>
2018-07-06 11:05:33 -0700

    Adds char constants

--- internal/char/character.go
+++ internal/char/character.go
@@ -1,5 +1,15 @@
 package char
 
+const (
+	DefaultCharacterScale  float32 = 2.0 // 32px -> 64px
+	DefaultCharacterSize   float32 = 80.0
+	DefaultCharacterZIndex float32 = 1.0
+
+	// These offset values center 64x64 characters in 80x80 tiles.
+	DefaultCharacterOffsetX float32 = 8.0
+	DefaultCharacterOffsetY float32 = 4.0
+)
+
 type Character interface {
 	GetName() string
 
