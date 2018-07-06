fc846fa104ff16ce4cfc7892764357d59cf9620d
Will Roberts <will.roberts11@gmail.com>
2018-07-06 10:49:00 -0700

    Adds the Character interface

---
+++ internal/character/character.go
@@ -0,0 +1,23 @@
+package character
+
+type Character interface {
+	GetName() string
+
+	GetX() int
+	SetX(int)
+
+	GetY() int
+	SetY(int)
+
+	GetHostility() bool
+	SetHostility(bool)
+
+	GetHitPoints() int
+	SetHitPoints(int)
+
+	GetDamage() int
+
+	GetExperiencePoints() int
+
+	Destroy()
+}
