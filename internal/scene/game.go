59e037bd10d079d6f3d9d70400f6797d3349cf36
Will Roberts <will.roberts11@gmail.com>
2018-07-06 10:39:59 -0700

    Ports tile map package

--- internal/scene/game.go
+++ internal/scene/game.go
@@ -4,6 +4,7 @@ import (
 	"image/color"
 
 	"github.com/willroberts/rpg/internal/grid"
+	"github.com/willroberts/rpg/internal/tmx"
 
 	"engo.io/ecs"
 	"engo.io/engo"
@@ -11,37 +12,65 @@ import (
 	"go.uber.org/zap"
 )
 
+const (
+	MapFile string = "maps/stone.tmx"
+)
+
 type GameScene struct {
 	Logger *zap.Logger
 	World  *ecs.World
+	Map    *tmx.Map
 	Grid   grid.Grid
 }
 
 func (scene *GameScene) Preload() {
 	scene.Logger.Info("starting preload")
+
+	// Map
+	if err := tmx.PreloadMap(MapFile); err != nil {
+		scene.Logger.Error("failed to preload map",
+			zap.String("err", err.Error()),
+		)
+		return
+	}
+
 	scene.Logger.Info("preload complete")
 }
 
 func (scene *GameScene) Setup(u engo.Updater) {
 	scene.Logger.Info("starting setup")
 
+	// World
 	w, ok := u.(*ecs.World)
 	if !ok {
 		scene.Logger.Error("world is not engo.Updater")
 	}
 	scene.World = w
 
-	// Replace these values with level.Width()/.Height().
-	scene.Grid = grid.NewGrid(10, 10)
+	// Map
+	m, err := tmx.LoadMap(MapFile)
+	if err != nil {
+		scene.Logger.Error("failed to load map",
+			zap.String("err", err.Error()),
+		)
+		return
+	}
+	scene.Map = m
 
+	// Grid
+	scene.Grid = grid.NewGrid(m.Level.Width(), m.Level.Height())
+
+	// RenderSystem
 	common.SetBackground(color.Black)
 	w.AddSystem(&common.RenderSystem{})
-
-	//for _, sys := range w.Systems() {
-	//	switch s := sys.(type) {
-	//	case *common.RenderSystem:
-	//	}
-	//}
+	for _, sys := range w.Systems() {
+		switch s := sys.(type) {
+		case *common.RenderSystem:
+			for _, t := range m.TileSet {
+				s.Add(&t.BasicEntity, &t.RenderComponent, &t.SpaceComponent)
+			}
+		}
+	}
 
 	scene.Logger.Info("setup complete")
 }
---
+++ internal/tmx/tmx.go
@@ -0,0 +1,65 @@
+package tmx
+
+import (
+	"engo.io/ecs"
+	"engo.io/engo"
+	"engo.io/engo/common"
+)
+
+const (
+	TileScale  float32 = 4.0
+	TileZIndex float32 = 0.0
+)
+
+type Map struct {
+	Level   *common.Level
+	TileSet []*Tile
+}
+
+type Tile struct {
+	ecs.BasicEntity
+	common.RenderComponent
+	common.SpaceComponent
+}
+
+func PreloadMap(filename string) error {
+	return engo.Files.Load(filename)
+}
+
+func LoadMap(filename string) (*Map, error) {
+	m := &Map{}
+	m.Level = &common.Level{}
+	m.TileSet = make([]*Tile, 0)
+
+	res, err := engo.Files.Resource(filename)
+	if err != nil {
+		return m, err
+	}
+
+	m.Level = res.(common.TMXResource).Level
+	for _, tl := range m.Level.TileLayers {
+		for _, t := range tl.Tiles {
+			if t.Image != nil {
+				tile := &Tile{
+					BasicEntity: ecs.NewBasic(),
+					RenderComponent: common.RenderComponent{
+						Drawable: t,
+						Scale: engo.Point{
+							X: TileScale,
+							Y: TileScale,
+						},
+					},
+					SpaceComponent: common.SpaceComponent{
+						Position: t.Point,
+						Width:    0,
+						Height:   0,
+					},
+				}
+				tile.RenderComponent.SetZIndex(TileZIndex)
+				m.TileSet = append(m.TileSet, tile)
+			}
+		}
+	}
+
+	return m, nil
+}
