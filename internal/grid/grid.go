5444556d19970f830dc9ce16e497f52d13384abb
Will Roberts <will.roberts11@gmail.com>
2018-07-06 16:34:40 -0700

    Adds CanMoveTo()

--- internal/grid/grid.go
+++ internal/grid/grid.go
@@ -2,6 +2,7 @@ package grid
 
 type Grid interface {
 	GetCell(int, int) Cell
+	CanMoveTo(int, int) bool
 }
 
 type grid struct {
@@ -12,6 +13,22 @@ type grid struct {
 
 func (g *grid) GetCell(x, y int) Cell {
 	return g.cells[y][x]
+}
+
+func (g *grid) CanMoveTo(x, y int) bool {
+	if x >= g.width || x < 0 {
+		return false
+	}
+
+	if y >= g.height || y < 0 {
+		return false
+	}
+
+	if !g.GetCell(x, y).IsTraversable() {
+		return false
+	}
+
+	return true
 }
 
 func NewGrid(w, h int) Grid {
