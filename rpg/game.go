89ec174e2732f8195704d1c898a6257c83117122
Will Roberts <will.roberts11@gmail.com>
2018-07-18 00:56:01 -0700

    Removes ported code

--- rpg/game.go
+++ rpg/game.go
@@ -1,119 +1,28 @@
 package rpg
 
 import (
-	"image/color"
 	"log"
 
-	"engo.io/ecs"
 	"engo.io/engo"
-	"engo.io/engo/common"
 )
 
-// gameScene is a pointer to the GameScene type in the global
-// namespace. This is currently stored this way so subsystems can
-// access shared resources like the combat log.
 var gameScene *GameScene
 
-// GameScene is our first and only scene at the moment. It includes the first
-// map, a static set of enemies, and only one room.
 type GameScene struct {
-	World      *ecs.World
-	Fonts      *FontSet
-	Sprites    *SpriteSet
-	Grid       *Grid
-	HUD        *HUD
-	Log        *ActivityLog
-	Player     *Player
-	EnemyTypes map[string]EnemyAttributes
+	Fonts *FontSet
+	HUD   *HUD
+	Log   *ActivityLog
 }
 
-// Preload validates and loads assets.
 func (scene *GameScene) Preload() {
-	log.Println("preloading")
-
-	// Store a pointer to our GameScene in the global namespace.
 	gameScene = scene
-
-	err := preloadMapAssets("maps/stone.tmx")
-	if err != nil {
-		log.Fatalln("failed to preload map assets:", err)
-	}
-
-	scene.Sprites, err = PreloadSprites()
-	if err != nil {
-		log.Fatalln("failed to preload sprites:", err.Error())
-	}
-
 	scene.Fonts, err = PreloadFonts()
 	if err != nil {
 		log.Fatalln("failed to preload fonts:", err.Error())
 	}
 }
 
-// Setup initializes all systems necessary for the game to function.
 func (scene *GameScene) Setup(u engo.Updater) {
-	// In EngoEngine/engo#513, ecs.World ws replaced with engo.Updater.
-	// This is how the authors updated their demos for compatibility.
-	w, _ := u.(*ecs.World)
-
-	log.Println("creating scene")
-	scene.World = w
-	common.SetBackground(color.Black)
-	w.AddSystem(&common.RenderSystem{})
-	w.AddSystem(&CameraSystem{})
-
-	log.Println("loading map")
-	level, tiles, err := loadMap("maps/stone.tmx")
-	if err != nil {
-		log.Fatalln("error:", err.Error())
-	}
-
-	log.Println("creating level grid")
-	scene.Grid = newGrid(level.Width(), level.Height())
-
-	log.Println("creating player")
-	gameScene.Player = newPlayer("Edmund", spriteWhiteZombie, 1, 1)
-
-	log.Println("creating enemies")
-	if err = loadEnemyTypes(); err != nil {
-		log.Fatalln("error:", err.Error())
-	}
-	enemies := []*Enemy{
-		newEnemy("Skeleton", spriteSkeleton, 2, 7),
-		newEnemy("Skeleton", spriteSkeleton, 8, 6),
-		newEnemy("Skeleton", spriteSkeleton, 5, 5),
-		newEnemy("Goblin", spriteGoblin, 4, 11),
-		newEnemy("Goblin", spriteGoblin, 7, 12),
-		newEnemy("Bear", spriteBear, 6, 17),
-		newEnemy("Demon", spriteDemon, 10, 22),
-	}
-
-	log.Println("configuring systems")
-	for _, sys := range w.Systems() {
-		switch s := sys.(type) {
-		case *common.RenderSystem:
-			log.Println("configuring render system")
-			for _, t := range tiles {
-				s.Add(&t.BasicEntity, &t.RenderComponent, &t.SpaceComponent)
-			}
-			s.Add(&gameScene.Player.BasicEntity, &gameScene.Player.RenderComponent, &gameScene.Player.SpaceComponent)
-			for _, e := range enemies {
-				s.Add(&e.BasicEntity, &e.RenderComponent, &e.SpaceComponent)
-			}
-		case *CameraSystem:
-			log.Println("configuring camera system")
-			s.Add(&gameScene.Player.BasicEntity, &gameScene.Player.CameraComponent,
-				&gameScene.Player.SpaceComponent)
-		}
-	}
-
-	log.Println("configuring camera")
-	w.AddSystem(&common.EntityScroller{
-		SpaceComponent: &gameScene.Player.SpaceComponent,
-		TrackingBounds: level.Bounds(),
-	})
-
-	log.Println("creating hud")
 	gameScene.HUD, err = newHUD()
 	if err != nil {
 		log.Fatalln("error:", err.Error())
@@ -122,14 +31,4 @@ func (scene *GameScene) Setup(u engo.Updater) {
 	scene.Log.Update("Welcome to the game.")
 	scene.Log.Update("There are three skeletons near you.")
 	scene.Log.Update("Try moving into them to attack.")
-
-	log.Println("binding controls")
-	bindControls()
-	log.Println("use the arrow keys to move")
-}
-
-// Type returns the name of the scene. This is used to satisfy engo's Scene
-// interface.
-func (scene *GameScene) Type() string {
-	return "GameScene"
 }
--- rpg/player.go
+++ rpg/player.go
@@ -1,36 +1,17 @@
 package rpg
 
 import (
-	"log"
-
-	"engo.io/ecs"
 	"engo.io/engo"
 	"engo.io/engo/common"
 )
 
 const failedMovementMsg string = "You can't go that way!"
 
-// Player is the player-controlled entity in the game.
 type Player struct {
-	ecs.BasicEntity
-	common.RenderComponent
-	common.SpaceComponent
-	CameraComponent
-
-	Name            string
-	HitPoints       int
-	MaxHitPoints    int
-	Level           int
 	Experience      int
 	ExperienceTable map[int]int
-
-	X, Y int
 }
 
-// Destroy removes a Player entity from the game. When the player dies, we
-// replace the character texture and stop processing incoming movement commands.
-// Removes the Player from the CameraSystem and the RenderSystem, and then re-add
-// the Player to the RenderSystem as a gravestone.
 func (p *Player) Destroy() {
 	for _, sys := range gameScene.World.Systems() {
 		switch s := sys.(type) {
@@ -48,47 +29,6 @@ func (p *Player) Destroy() {
 	}
 }
 
-// GetDamage returns the damage dealt by the Player.
-func (p *Player) GetDamage() int { return 1 }
-
-// GetHitPoints returns the current HP for the Player.
-func (p *Player) GetHitPoints() int { return p.HitPoints }
-
-// GetMaxHitPoints returns the maximum HP for the Player.
-func (p *Player) GetMaxHitPoints() int { return p.MaxHitPoints }
-
-// GetLevel returns the current Level of the Player.
-func (p *Player) GetLevel() int { return p.Level }
-
-// GetExperience returns the current XP for the Player.
-func (p *Player) GetExperience() int { return p.Experience }
-
-// IsHostile always returns false for the Player.
-func (p *Player) IsHostile() bool { return false }
-
-// GetName returns the name of the Player.
-func (p *Player) GetName() string { return p.Name }
-
-// GetXPBonus is stubbed for Player entities. Only needed for Enemy entities, but
-// we include it here to satisfy the Character interface.
-func (p *Player) GetXPBonus() int { return 0 }
-
-// GetX returns the Player's X coordinate.
-func (p *Player) GetX() int { return p.X }
-
-// GetY returns the Player's Y coordinate.
-func (p *Player) GetY() int { return p.Y }
-
-// ModifyHitPoints adds a number to the Player's hit points. To deal damage to
-// the Player, provide a negative number.
-func (p *Player) ModifyHitPoints(amount int) {
-	p.HitPoints += amount
-	if p.HitPoints < 0 {
-		p.HitPoints = 0
-	}
-}
-
-// ModifyExperience adds a number to the Player's experience points.
 func (p *Player) ModifyExperience(amount int) {
 	p.Experience += amount
 	if p.Experience >= p.ExperienceTable[p.Level+1] {
@@ -96,56 +36,10 @@ func (p *Player) ModifyExperience(amount int) {
 	}
 }
 
-// LevelUp increases the Player's level, increases MaxHitPoints, and restores HP.
 func (p *Player) LevelUp() {
 	gameScene.Log.Update("Level up!")
 	p.Level++
 	p.Experience = 0
 	p.MaxHitPoints += 2
 	p.HitPoints = p.MaxHitPoints
-}
-
-// SetHostility has no effect on the Player.
-func (p *Player) SetHostility(h bool) {}
-
-// SetX updates the Player's X coordinate.
-func (p *Player) SetX(x int) { p.X = x }
-
-// SetY updates the Player's Y coordinate.
-func (p *Player) SetY(y int) { p.Y = y }
-
-// newPlayer creates and returns a Player.
-func newPlayer(name string, spriteIndex, x, y int) *Player {
-	p := &Player{
-		BasicEntity: ecs.NewBasic(),
-		CameraComponent: CameraComponent{
-			SchemeHoriz: "horizontal",
-			SchemeVert:  "vertical",
-		},
-		X:               x,
-		Y:               y,
-		Name:            name,
-		HitPoints:       20,
-		MaxHitPoints:    20,
-		Level:           1,
-		Experience:      0,
-		ExperienceTable: defaultPlayerExperienceTable,
-	}
-	p.RenderComponent = common.RenderComponent{
-		Drawable: gameScene.Sprites.Characters.Cell(spriteIndex),
-		Scale:    engo.Point{X: charScale, Y: charScale},
-	}
-	p.RenderComponent.SetZIndex(zCharacter)
-	p.SpaceComponent = common.SpaceComponent{
-		Position: engo.Point{
-			X: (charSizeX * float32(x)) + charOffsetX,
-			Y: (charSizeY * float32(y)) + charOffsetY,
-		},
-		Width:  charSizeX,
-		Height: charSizeY,
-	}
-	if err := gameScene.Grid.AddOccupant(p, x, y); err != nil {
-		log.Println("failed to add occupant to grid:", err)
-	}
-	return p
 }
